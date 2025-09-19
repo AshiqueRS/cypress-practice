d4a58a5f3002c867c182cf12bfb6942477d4b373
AshiqueRS <ashiq.cse24@gmail.com>
2025-09-19 12:48:54 +0600

    Check dropdown in Account Details

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,6 +4,10 @@ describe('Registration Functionality',()=>{
         cy.get("input[name='username']").type("USER")
         cy.get("input[name='password']").type("1234")
         cy.get("input[class='button']").click()
+        cy.get("#accountTable > tbody > tr:nth-child(1) > td:nth-child(1) > a").click()
+        cy.get("#month").select(1)
+        cy.get("#transactionType").select(1)
+        cy.get("input[value='Go']").click()
 
     })
 })
